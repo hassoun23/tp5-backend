0546f823c3f1db9b33ecc94ac418da7d5c532261
Hassan Hamze <Hassan2393@gmail.com>
2022-09-15 19:53:16 -0300

    tp5 pug

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const { request } = require('express');
-const handlebars = require('express-handlebars');
+
 const express = require('express');
 const app = express();
 const Router = express.Router();
@@ -10,10 +10,9 @@ const server = app.listen(8080, () => console.log('server up!'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.engine('handlebars', handlebars.engine());
 app.set('views', './views');
 //app.set('view engine', 'ejs');
-app.set('view engine', 'handlebars');
+app.set('view engine', 'pug');
 
 app.get('/', (req, res) => {
 	res.render('create-product');
