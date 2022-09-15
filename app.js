e7d8b227d69264015c7fce35aed7ecc3c491be4c
Hassan Hamze <Hassan2393@gmail.com>
2022-09-15 19:13:01 -0300

    tp 5 handelbar

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const { request } = require('express');
+const handlebars = require('express-handlebars');
 const express = require('express');
 const app = express();
 const Router = express.Router();
@@ -9,8 +10,10 @@ const server = app.listen(8080, () => console.log('server up!'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.engine('handlebars', handlebars.engine());
 app.set('views', './views');
-app.set('view engine', 'ejs');
+//app.set('view engine', 'ejs');
+app.set('view engine', 'handlebars');
 
 app.get('/', (req, res) => {
 	res.render('create-product');
